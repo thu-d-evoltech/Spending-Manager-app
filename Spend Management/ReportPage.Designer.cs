5cc2266d8a510e7d894125085e655b96ba65ecfa
thu-d <thu-d@evoltech.co.jp>
2025-06-16 09:22:28 +0900

    レポートページ

--- Spend Management/ReportPage.Designer.cs
+++ Spend Management/ReportPage.Designer.cs
@@ -28,17 +28,61 @@
         /// </summary>
         private void InitializeComponent()
         {
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
             SuspendLayout();
             // 
-            // Report
+            // button1
+            // 
+            button1.BackColor = Color.FromArgb(255, 192, 128);
+            button1.Font = new Font("Yu Gothic UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 128);
+            button1.Location = new Point(41, 607);
+            button1.Name = "button1";
+            button1.Size = new Size(208, 61);
+            button1.TabIndex = 0;
+            button1.Text = "収支グラフ";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.FromArgb(255, 192, 128);
+            button2.Font = new Font("Yu Gothic UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 128);
+            button2.Location = new Point(290, 607);
+            button2.Name = "button2";
+            button2.Size = new Size(208, 61);
+            button2.TabIndex = 1;
+            button2.Text = "支出グラフ";
+            button2.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            button3.BackColor = Color.FromArgb(255, 192, 128);
+            button3.Font = new Font("Yu Gothic UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 128);
+            button3.Location = new Point(534, 607);
+            button3.Name = "button3";
+            button3.Size = new Size(208, 61);
+            button3.TabIndex = 2;
+            button3.Text = "Excelエクスポート";
+            button3.UseVisualStyleBackColor = false;
+            // 
+            // ReportPage
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            Name = "Report";
-            Size = new Size(914, 589);
+            BackColor = SystemColors.GradientInactiveCaption;
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Name = "ReportPage";
+            Size = new Size(793, 708);
             ResumeLayout(false);
         }
 
         #endregion
+
+        private Button button1;
+        private Button button2;
+        private Button button3;
     }
 }
